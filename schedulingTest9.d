5acb6fc35bd79e042382634ce38456673824811e
weilern1121 <you@example.com>
2019-03-28 13:51:51 +0200

    fix bugs

--- schedulingTest9.d
+++ schedulingTest9.d
@@ -1,5 +1,5 @@
-pathTest.o: Project_Test/XV6_Tests/pathTest.c /usr/include/stdc-predef.h \
- Project_Test/XV6_Tests/../../param.h \
+schedulingTest9.o: Project_Test/XV6_Tests/schedulingTest9.c \
+ /usr/include/stdc-predef.h Project_Test/XV6_Tests/../../param.h \
  Project_Test/XV6_Tests/../../types.h Project_Test/XV6_Tests/../../stat.h \
  Project_Test/XV6_Tests/../../user.h Project_Test/XV6_Tests/../../fs.h \
  Project_Test/XV6_Tests/../../fcntl.h \
